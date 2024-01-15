126336ac79ce340f6ac2bf3e69c8148085ef433f
abhilasha <abhilashaduwal@gmail.com>
2024-01-15 16:47:13 +0545

    feat(react-app): created basic UI and fetch API for client

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,31 @@
-import './App.css';
+import "./App.css";
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Success from "./pages/Success";
+import { useSelector } from "react-redux";
 
 function App() {
+  const user = useSelector((state) => state.user.currentUser);
   return (
-  <h1>MY react app</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ react-app/src/data.js
@@ -0,0 +1,80 @@
+export const sliderItems = [
+    {
+        id:1,
+        img: "https://qwestore.com/png_images_min/7/bWoman-shopping-Imageb-Women39s-Clothes-4341.png",
+        title:"SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id:2,
+        img: "https://png.pngtree.com/png-vector/20231222/ourmid/pngtree-young-model-with-winter-clothes-png-image_11182033.png",
+        title:"WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id:3,
+        img: "https://hdpng.com/images/clothing-hd-png-model-png-hd-png-image-600.png",
+        title:"AUTUMN SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "faa796",
+    }
+
+]
+
+export const categories = [
+    {
+        id:1,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRxakivpbWBLH7ZlXEI5RDSqd-2L5n4NFIVg&usqp=CAU",
+        title:"SHIRT STYLE!",
+        category: "woman"
+    },
+    {
+        id:2,
+        img: "https://cms.eliza.co.uk/wp-content/uploads/2022/05/SEI_107328245.jpg?w=1024",
+        title:"LOUNGEWEAR LOVE",
+        category: "coats"
+    },
+    {
+        id:3,
+        img: "https://static-01.daraz.com.np/p/0020ff267490bcaa8da380edf4a4406a.jpg",
+        title:"LIGHT JACKETS",
+        category: "denim"
+    }
+]
+
+export const popularProducts = [
+    {
+        id:1,
+        img:"https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/449879/item/goods_08_449879.jpg?width=750"
+    },
+    {
+        id:2,
+        img:"https://www.dolcegabbana.com/dw/image/v2/BKDB_PRD/on/demandware.static/-/Sites-15/default/dwce9f688c/images/zoom/F6HAJTFSA6C_HN4YH_0.jpg?sw=173&sh=238"
+    },
+    {
+        id:3,
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDxS-TkJU-LFYTKyUSFCDfHDfarByETBnG2Q&usqp=CAU"
+    },
+    {
+        id:4,
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPZUd0eEnM9MtNH9Fl56S4WCSgjikYoi0P6Q&usqp=CAU"
+    },
+    {
+        id:5,
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJgB7Um5oSiwVo2P-uqkflViq6apXUVx7JyQ&usqp=CAU"
+    },
+    {
+        id:6,
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiLjTK--mL5JGnyz-3_kVJCcTwPi6-8vAV1ASGzSjQ3g&s"
+    },
+    {
+        id:7,
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPZUd0eEnM9MtNH9Fl56S4WCSgjikYoi0P6Q&usqp=CAU"
+    },
+    {
+        id:8,
+        img:"https://static-01.daraz.com.np/p/bf33a6736db278e1c38ebacbde1dde44.jpg_300x0q75.webp"
+    },
+]
